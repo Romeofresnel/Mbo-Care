ca86e1c11f5f570c30839b3da27546f0ce4b73e6
romeo fresnel <romeofresnel@gmail.com>
2025-07-27 05:00:10 +0200

    version 1.0.1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import './index.scss'
 import { Provider } from "react-redux";
 import store from "./redux/Store.jsx";
 // import { BrowserRouter } from "react-router-dom";
